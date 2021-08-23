29927a3f3e16a9a8f3b63d7d925c6a3abca7c839
Suguivy <solis@mailbox.org>
2021-08-23 18:59:51 +0200

    Added assignments (some defined by default) and changed evaluation strategy to a stricter one

--- README.md
+++ README.md
@@ -1,44 +1,82 @@
 # lambda-playground
 
-A simple REPL to experiment with the untyped lambda calculus I made in one night. I made it for me to play with it and understand it better.
+A simple REPL to experiment with the untyped lambda calculus. I made it for me to play with it and understand it better while reading the *Types and Programming Languages* book.
 
-I'm planning to improve it and add some more features.
+## Syntax and usage
 
-## Syntax
-The syntax is very simple. There are only three types of terms, which are **variables**, **abstractions** and **applications**:
+The syntax is very simple. There are only three types of terms, which are **variables**, **abstractions** and **applications**, and two types of statements, **evaluations** and **assignements**.
 
-To write a **variable**, simply put a name, composed of letters only:
+**Variables**
+
 ```
 x
+foo
+bar
 ```
 
-To write an **abstraction**, write the function but instead of the lambda, use the `\\` symbol:
+**Abstractions**
 
 ```
 \x.x
-\x.\y. x y
+\x.x a -- Equals to \x.(x a)
+\x.\y.x y
 ```
 
-To write an **application**, simply put the two terms one after the other:
+**Applications**
 
 ```
+(\x.x) a
 (\x.x) (\y.y)
+a (b c)
+a b c -- Equals to (a b) c
 ```
 
-You can use parenthesis to indicate how to apply terms.
+Note that you can use parenthesis to indicate how the interpreter should evaluate terms.
 
-## Usage
+### Evaluations
 
-The interpreter will show, step by step, how the expresion is evaluated, until reach the normal form:
+To evaluate a term, simply write it directly. The interpreter will evaluate it until reaching the normal form
 
 ```
-λ> (\x.x) (\y.(a b c (c d)))
-(λx.x) (λy.a b c (c d))
-(λy.a b c (c d))
-λ> (\x.x) (\y.y) (\z.z) (\a.a)  
-(λx.x) (λy.y) (λz.z) (λa.a)
-(λy.y) (λz.z) (λa.a)
-(λz.z) (λa.a)
-(λa.a)
+λ> (\x.x) a
+(λx.x) a
+a
 ```
 
+### Assignments
+
+You can also assign names to terms, to avoid repeating yourself. Names can be used inside terms, and they will be converted to the corresponding term before evaluation, as long as the name is not bound by an outer abstraction.
+
+```
+λ> id = \x.x
+λ> zero = \s.\z.s z
+λ> id zero
+(λx.x) (λs.λz.z)
+λs.λz.z
+```
+
+There are already some builtin names, and I will add some more in the future:
+
+```
+id    = λx.x
+true  = λt.λf.t
+false = λt.λf.f
+zero  = λs.λz.z
+one   = λs.λz.s z
+two   = λs.λz.s (s z)
+succ  = λn.λf.λx.f (n f x)
+```
+
+## Instalation
+
+You must install some Haskell related tools, specifically `stack`. If you have it already installed, open a terminal and do the following steps:
+
+``` sh
+git clone https://github.com/Suguivy/lambda-playground.git
+cd lambda-playground
+stack build
+```
+
+If you want to have it installed in your environment to run it easily as a command (storing it in `/home/<user>/bin`), run `stack install`. After that run the interpreter with `lambda-playground`.
+
+If you only want to run it, run `stack run` in the cloned folder.
--- src/Main.hs
+++ src/Main.hs
@@ -2,34 +2,77 @@ module Main where
 
 import Text.ParserCombinators.Parsec
 import Control.Applicative hiding ((<|>))
+import Data.Maybe
 import System.IO
 
 data Term = Var String
           | Abs String Term
           | App Term Term
 
+data Statement = Assignment String Term
+               | Evaluation Term
+
+type Env = [(String, Term)]
+
 instance Show (Term) where
-  show (Var v) = v
-  show (Abs v b) = "(λ" ++ v ++ "." ++ show b ++ ")"
-  show (App t1 t2@(App _ _)) = show t1 ++ " (" ++ show t2 ++ ")"
-  show (App t1 t2) = show t1 ++ " " ++ show t2
+  show (Abs v b) = "λ" ++ v ++ "." ++ show b
+  show t = show' t
+    where show' (Var v) = v
+          show' (Abs v b) = "(λ" ++ v ++ "." ++ show b ++ ")"
+          show' (App t1 t2@(App _ _)) = show' t1 ++ " (" ++ show' t2 ++ ")"
+          show' (App t1 t2) = show' t1 ++ " " ++ show' t2
+
+------------------------------------------------------------
+
 main :: IO ()
-main = loop
-  where loop = do putStr "λ> "
-                  hFlush stdout
-                  s <- getLine
-                  case (parse term "error" s) of
-                    Left err -> putStrLn $ show err
-                    Right t  -> evalAndPrint t
-                  loop
+main = loop [ ("id"    , (Abs "x" (Var "x")))
+            , ("true"  , (Abs "t" (Abs "f" (Var "t"))))
+            , ("false" , (Abs "t" (Abs "f" (Var "f"))))
+            , ("zero"  , (Abs "s" (Abs "z" (Var "z"))))
+            , ("one"   , (Abs "s" (Abs "z" (App (Var "s") (Var "z")))))
+            , ("two"   , (Abs "s" (Abs "z" (App (Var "s") (App (Var "s") (Var "z"))))))
+            , ("succ"  , (Abs "n" (Abs "f" (Abs "x" (App (Var "f") (App (App (Var "n") (Var "f")) (Var "x")))))))
+            ]
+  where loop :: Env -> IO ()
+        loop env = do putStr "λ> "
+                      hFlush stdout
+                      s <- getLine
+                      case (parse statement "error" s) of
+                        Left err -> putStrLn $ show err
+                        Right st -> case st of
+                          (Assignment v t) -> loop ((v,t):env)
+                          (Evaluation t)   -> evalAndPrint (replaceEnv env t) >> loop env
 
 {-
+
+Informal grammar:
+
+Statement ::= Assignment | Evaluation
+Assignment ::= String "=" Term
+Evaluation ::= Term
 Term ::= App | Var | Abs | "(" Term ")"
 Var  ::= /[a-z]+/
 Abs  ::= "\" /[a-z]+/ "." Term
 App  ::= NoApp Term
 NoApp  ::= Var | Abs | "(" Term ")"
 -}
+
+
+statement :: Parser Statement
+statement = do st <- spaces >> (try assignment <|> evaluation)
+               eof
+               return st
+
+
+assignment :: Parser Statement
+assignment = do v <- many1 letter
+                spaces
+                _ <- char '='
+                t <- term
+                return $ Assignment v t
+
+evaluation :: Parser Statement
+evaluation = Evaluation <$> term
 
 term :: Parser Term
 term = spaces >> (try termApp <|> termVar <|> termAbs <|> termParen)
@@ -64,14 +107,52 @@ termVar = do v <- many1 letter
 
 ------------------------------------------------------------
 
+replaceEnv :: Env -> Term -> Term
+replaceEnv [] t = t
+replaceEnv env t@(Var x) = fromMaybe t (x `lookup` env)
+replaceEnv env (App t1 t2) = App (replaceEnv env t1) (replaceEnv env t2)
+replaceEnv env (Abs n b) = (Abs n (replaceEnv envWithoutBinded b))
+  where envWithoutBinded = filter ((/= n) . fst) env
+
 eval :: Term -> Term
 eval t = maybe t eval (evalStep t)
 
+{-
+
+The interpreter follows a strict evaluation strategy,
+based on the following rules:
+
+   t2 -> t2'
+---------------
+t1 t2 -> t1 t2'
+
+   t1 -> t1'
+---------------
+t1 t2 -> t1' t2
+
+-------------------------
+(\v.tb) t2 -> [v -> t2]tb
+
+        tb -> tb'
+-------------------------
+   (\v.tb) -> (\v.tb')
+
+-}
+
+isNormal :: Term -> Bool
+isNormal (Var _) = True
+isNormal (Abs _ tb) = isNormal tb
+isNormal (App (Abs _ _) _) = False
+isNormal (App t1 t2) = isNormal t1 && isNormal t2
+
 evalStep :: Term -> Maybe Term
-evalStep (App (Abs v1 b1) t2@(Abs _ _)) = Just $ substitute v1 t2 b1
-evalStep (App t1@(Abs _ _) t2) = liftA2 App (pure t1) (evalStep t2)
-evalStep (App t1 t2) = liftA2 App (evalStep t1) (pure t2)
-evalStep _ = Nothing
+evalStep (Abs n tb) = liftA2 Abs (pure n) (evalStep tb)
+evalStep (App (Abs n tb) t2) = Just $ substitute n t2 tb
+evalStep (App t1 t2)
+  | not $ isNormal t2 = liftA2 App (pure t1) (evalStep t2)
+  | not $ isNormal t1 = liftA2 App (evalStep t1) (pure t2)
+  | otherwise         = Nothing
+evalStep (Var _) = Nothing
 
 substitute :: String -> Term -> Term -> Term
 substitute s t (Var n) = if s == n then t else (Var n)
